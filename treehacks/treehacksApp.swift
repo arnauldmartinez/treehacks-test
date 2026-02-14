f8552bcb8db3fda469ff2ff5f875e25b8fd6a1f3
arnauldmartinez <arnauldmartinez@gmail.com>
2026-02-13 23:55:16 -0800

    Initial Commit

---
+++ treehacks/treehacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  treehacksApp.swift
+//  treehacks
+//
+//  Created by Arnauld Martinez on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct treehacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
