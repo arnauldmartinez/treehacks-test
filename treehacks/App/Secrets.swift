01f105d049b24be0de37f20600f59f16e8f40835
Arnauld Martinez <43526322+arnauldmartinez@users.noreply.github.com>
2026-02-15 08:28:59 -0800

    Update Secrets.swift

--- treehacks/App/Secrets.swift
+++ treehacks/App/Secrets.swift
@@ -1,5 +1,4 @@
 import Foundation
 
 enum Secrets {
-    static let openAIKey = "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"
 }
